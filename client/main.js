862fb142b13467b4298d0124fc9f1a2f84969224
unknown <vmiriga@gmail.com>
2020-11-28 18:07:15 +0300

    Implemented deletion functionality

--- client/main.js
+++ client/main.js
@@ -36,9 +36,9 @@ Template.output.events({
     // console.log(doc[0]._id);
     // editor.render(doc[0].body);
   },
-  "click #newDoc":function(event){
-    Session.set("docid", null);
-  },
+  // "click #newDoc":function(event){
+  //   Session.set("docid", null);
+  // },
 });
 Template.info.events({
   
@@ -48,6 +48,7 @@ Template.info.events({
 Template.editor.events({
   "click #saveButton": async function(event){
     event.preventDefault();
+    var mode = Session.get("mode");
     var title = $('#title-text-input').val();
     // var title = "Sample4 Doc";
 
@@ -60,9 +61,17 @@ Template.editor.events({
     //   console.log('Saving failed: ', error)
     // });
     // console.log(body);
-    Meteor.call('addDocument',title, body, function(){
-      console.log('addDocument called' + title + body);
-    });
+    if (mode == "create") {
+      Meteor.call('addDocument',title, body, function(){
+        console.log('addDocument called' + title + body);
+      });
+    } else {
+      var id = Session.get("docid");
+      Meteor.call('updateDocument', id, title, body, function(){
+        console.log('updateDocument called', id, + title + body);
+      });
+    }
+    
     return false;
     
   },
@@ -87,7 +96,15 @@ Template.editor.events({
       }
     );
   },
-});
+
+  "click #deleteButton": function () {
+    var id = Session.get("docid");
+      Meteor.call('deleteDocument', id, function(){
+        console.log('deleteDocument called', id);
+      });
+  },
+}
+);
 
 
 Template.editor.onRendered(function(){
@@ -207,105 +224,7 @@ Template.editor.onRendered(function(){
     /**
      * Initial Editor data
      */
-    // data: {
-    //   blocks: [
-    //     {
-    //       type: "header",
-    //       data: {
-    //         text: "Editor.js",
-    //         level: 2
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.'
-    //       }
-    //     },
-    //     {
-    //       type: "header",
-    //       data: {
-    //         text: "Key features",
-    //         level: 3
-    //       }
-    //     },
-    //     {
-    //       type : 'list',
-    //       data : {
-    //         items : [
-    //           'It is a block-styled editor',
-    //           'It returns clean data output in JSON',
-    //           'Designed to be extendable and pluggable with a simple API',
-    //         ],
-    //         style: 'unordered'
-    //       }
-    //     },
-    //     {
-    //       type: "header",
-    //       data: {
-    //         text: "What does it mean «block-styled editor»",
-    //         level: 3
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.'
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : `There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.`
-    //       }
-    //     },
-    //     {
-    //       type: "header",
-    //       data: {
-    //         text: "What does it mean clean data output",
-    //         level: 3
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : 'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below'
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : `Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.`
-    //       }
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : 'Clean data is useful to sanitize, validate and process on the backend.'
-    //       }
-    //     },
-    //     {
-    //       type : 'delimiter',
-    //       data : {}
-    //     },
-    //     {
-    //       type : 'paragraph',
-    //       data : {
-    //         text : 'We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make its core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏'
-    //       }
-    //     },
-    //     {
-    //       type: 'image',
-    //       data: {
-    //         url: 'assets/onepiece.jpg',
-    //         caption: '',
-    //         stretched: false,
-    //         withBorder: true,
-    //         withBackground: false,
-    //       }
-    //     },
-    //   ]
-    // },
+    // data: {},
     onReady: async function(){
       var docId = Session.get("docid");
     // $(event.target).closest('#item').attr("data-id");
@@ -327,4 +246,18 @@ Template.editor.onRendered(function(){
   })
     console.log('DOM is ready');
     return false;
+});
+
+Template.editor.helpers({
+  editing: function() {
+    var editingMode;
+    if (Session.get("mode") == "edit") {
+      editingMode = true;
+      console.log('edit is true');
+    } else {
+      editingMode = false;
+      console.log('edit is false');
+    }
+    return editingMode;
+  }
 });
--- shared/main.js
+++ shared/main.js
@@ -10,12 +10,34 @@ Meteor.methods({
                 owner: Meteor.users.findOne({ _id: Meteor.userId() }).username,
                 createdOn:new Date(),
                 title:title,
-                body:body
+                body: body
             };
             return Documents.insert(doc);
         } else {
             alert("You need to login to create a document");
         }
+    },
+    updateDocument:function(id, title, body) {
+        if(this.userId){//logged in user
+            // var doc = Documents.find({_id:id}).fetch();
+            return Documents.update({_id:id}, {
+                title: title,
+                body: body
+            });
+        } 
+        // else {
+        //     alert("You need to login to update a document");
+        // }
+    },
+
+    deleteDocument:function(id) {
+        if(this.userId){//logged in user
+            // var doc = Documents.find({_id:id}).fetch();
+            return Documents.remove({_id:id});
+        } 
+        // else {
+        //     alert("You need to login to update a document");
+        // }
     },
 }
 
